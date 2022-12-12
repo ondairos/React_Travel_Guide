50ebbee72b1ecc644e5a38cef2d0dca2a394fa15
Ondairos <ondairos@gmail.com>
2022-12-12 17:49:42 +0200

    MainCard CSS flexbox image and info

--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -2,24 +2,32 @@ import React from "react";
 
 export const MainCard = () => {
   return (
-    <div className="main__card">
-      <img className="main__card-image" src="" alt="placeholder image" />
+    <>
+      <div className="main__card">
+        <div class="column__image">
+          <img
+            className="main__card-image"
+            src="public/photo1.jpg"
+            alt="placeholder image"
+          />
+        </div>
 
-      <div className="main__span">
-        <span className="main__card-country">country</span>{" "}
-        <span className="main__card-gmaps">View On Google Maps</span>
+        <div class="column">
+          <h3 className="main__card-country">Country</h3>
+          <a className="main__card-gmaps">View On Google Maps</a>
+
+          <h1 className="main__card-title">Title</h1>
+          <h3 className="main__card-duration">12 Jan,2021 - 25 Jan, 2021</h3>
+          <p className="main__card-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            ratione repudiandae accusamus rem! Aspernatur harum natus
+            accusamus.Dolorum ratione repudiandae accusamus rem! Aspernatur
+            harum natus accusamus.
+          </p>
+        </div>
       </div>
-
-      <h1 className="main__card-title">Title</h1>
-      <h3 className="main__card-duration">12 Jan,2021 - 25 Jan, 2021</h3>
-      <p className="main__card-description">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ratione
-        repudiandae accusamus rem! Aspernatur harum natus accusamus tenetur
-        ducimus eum et nesciunt, dolores, culpa fugit, mollitia molestiae error
-        libero optio?
-      </p>
       <hr />
-    </div>
+    </>
   );
 };
 
