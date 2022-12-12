4adee989259887b25bee4e914d419a7a6a80c7df
Ondairos <ondairos@gmail.com>
2022-12-12 16:09:04 +0200

    added navbar title and logo(feather)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import * as Icon from 'react-feather';
 
 export const Navbar = () => {
   return (
-    <div className='main__nav'>my travel journal.</div>
-  )
-}
+    <div className="main__nav">
+      <Icon.Map fill="white" />
+      <p className="main__title">my travel journal.</p>
+      
+    </div>
+  );
+};
 
 export default Navbar;
